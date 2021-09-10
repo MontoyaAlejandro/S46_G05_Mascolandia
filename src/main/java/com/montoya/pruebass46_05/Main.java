076f319e23bdaa0f91473894056982f51939df61
Programador <Programador@Programing>
2021-09-10 14:42:21 -0500

    Pruebas iniciales del proyecto

---
+++ src/main/java/com/montoya/pruebass46_05/Main.java
@@ -0,0 +1,21 @@
+package com.montoya.pruebass46_05;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+      /* 
+        este un paquete reral comn la estructura quye necesitaremos para desarrollar nuestro
+        proyecto,   espero sea de ayuda, 
+        Les deseo un maravillosos dia.
+        
+      */  
+      
+      System.out.println("Bienvenidos Compañeros");
+      
+    }
+    
+}
